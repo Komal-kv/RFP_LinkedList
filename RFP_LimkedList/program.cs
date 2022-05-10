b226fcefc6a6e37eaa8805e811bd9eae73db8a0e
Komal <divyavairagade@gmail.com>
2022-05-10 12:55:53 +0530

    Add project files.

---
+++ RFP_LimkedList/program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFP_LinkedList
+{
+    internal class program
+    {
+        public static void Main()
+        {
+            LinkedList list = new LinkedList();
+
+            list.addFirst("70"); list.addFirst("30"); list.addFirst("56"); list.printlist();
+
+            list.InsertAtParticularPosition(1, "40"); list.printlist();
+
+            //list.deleteLast("70");list.printlist();  //this will delete the last node
+
+            //list.deleteFirst("56"); list.printlist();  //this will delete the first node
+
+            //list.Search("70");list.printlist();
+
+        }
+    }
+}
